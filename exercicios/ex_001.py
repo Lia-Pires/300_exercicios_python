77c7f1a3e32150ee5d7505a6260da31f2e7bc2d2
Lia Pires <liasth.pires@gmail.com>
2022-11-10 10:12:27 -0300

    adding ex 01, 02 and 03

---
+++ exercicios/ex_001.py
@@ -0,0 +1,7 @@
+'''
+1- Crie três variáveis com três tipos de dados diferentes, respeitando sua sintaxe:
+'''
+
+nome = 'Lia'
+ano = 1992
+valor = 20.05
---
+++ exercicios/ex_002.py
@@ -0,0 +1,5 @@
+'''
+2 - Crie um comentário de ano máximo uma linha
+'''
+
+# Exemplo de comentário de uma linha
