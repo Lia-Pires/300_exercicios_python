8e307854194243429f58fe6dcd53f1978a38a679
Lia Pires <liasth.pires@gmail.com>
2022-11-11 09:16:07 -0300

    Adding ex 10

--- exercicios/ex_009.py
+++ exercicios/ex_009.py
@@ -3,3 +3,4 @@ Crie uma lista com 5 nomes de pessoas:
 '''
 
 names = ['Giovanna', 'Caio', 'Pedro', 'Ana', 'Álvara']
+print(names)
---
+++ exercicios/ex_010.py
@@ -0,0 +1,9 @@
+'''
+Mostre o tamanho da lista de nomes /
+o número de elementros da lista de nomes: 
+Mostre separadamente apenas o terceiro elemento dessa lista:
+'''
+
+names = ['Giovanna', 'Caio', 'Pedro', 'Ana', 'Álvara']
+print(len(names))
+print(names[2])
