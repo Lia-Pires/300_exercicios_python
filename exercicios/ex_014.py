8f94f1741f9737c21436c55280b5d9323cff0518
Lia Pires <liasth.pires@gmail.com>
2022-11-13 20:16:37 -0300

    Adding ex 14

---
+++ exercicios/ex_014.py
@@ -0,0 +1,12 @@
+"""
+14- Realize as operações de multiplicação e de divisão entre os valores das variáveis num1 e num:
+"""
+
+num1 = 52
+num2 = 106
+
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"A multiplicação de {num1} e {num2} é {multiplicacao}")
+print(f"A divisão de {num1} e {num2} é {divisao:.2f}")
