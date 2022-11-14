50f691d7f983d8e22187be49e1a2f59bc59b1a47
Lia Pires <liasth.pires@gmail.com>
2022-11-14 20:27:16 -0300

    Adding ex 16

---
+++ exercicios/ex_016.py
@@ -0,0 +1,12 @@
+"""
+Escreva um programa que pede que o usuário dê entrada em dois valores, em seguida, exiba em tela o resultado da soma, subtração, multiplicação e divisão desses números:
+"""
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+print(f"Os números digitados foram: {num1} e {num2}.")
+print(f"Sua soma é {num1 + num2}.")
+print(f"Sua subtração é {num1 - num2}.")
+print(f"Sua multiplicação é: {num1 * num2}.")
+print(f"E sua divisão é: {num1 / num2}.")
