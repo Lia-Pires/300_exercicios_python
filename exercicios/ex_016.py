27ae380aa124720c531b19d3c437ef91f6e4eece
Lia Pires <liasth.pires@gmail.com>
2022-11-14 20:33:27 -0300

    Adding ex 18

--- exercicios/ex_016.py
+++ exercicios/ex_016.py
@@ -1,5 +1,5 @@
 """
-Escreva um programa que pede que o usuário dê entrada em dois valores, em seguida, exiba em tela o resultado da soma, subtração, multiplicação e divisão desses números:
+16 - Escreva um programa que pede que o usuário dê entrada em dois valores, em seguida, exiba em tela o resultado da soma, subtração, multiplicação e divisão desses números:
 """
 
 num1 = int(input("Digite o primeiro número: "))
--- exercicios/ex_017.py
+++ exercicios/ex_017.py
@@ -1,5 +1,5 @@
 """
-Dada duas variáveis num1 e num2 com valores 100 e 89. respectivamente, verifique se o valor de num1 é maior que o de num2:
+17 - Dada duas variáveis num1 e num2 com valores 100 e 89. respectivamente, verifique se o valor de num1 é maior que o de num2:
 """
 num1 = 100
 num2 = 89
