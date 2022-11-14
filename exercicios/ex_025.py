e7d62a75fcc754e9403affa09b9e34ec5501be52
Lia Pires <liasth.pires@gmail.com>
2022-11-14 20:49:25 -0300

    Adding ex 25

---
+++ exercicios/ex_025.py
@@ -0,0 +1,10 @@
+'''
+25 - Peça para que o usuário digite um número, em seguida exiba em tela uma mensagem dizendo se tal número é PAR ou ÍMPAR:
+'''
+
+num = int(input("Digite um número: "))
+
+if (num % 2) == 0:
+    print(f"O número digitado ({num}) é PAR.")
+else:
+    print(f"O número digitado ({num}) é ÍMPAR.")
