2032c348e50e3bd54088a141b4d0965c4bc06415
Lia Pires <liasth.pires@gmail.com>
2022-11-15 15:02:42 -0300

    Adding ex 30

---
+++ exercicios/ex_030.py
@@ -0,0 +1,10 @@
+''' 
+30 - Crie um programa que realiza a contagem de 0 a 20, exibindo apenas os números pares:
+'''
+
+# for i in range(21):
+#     if (i % 2 == 0):
+#         print(i)
+
+for i in range(0, 21, 2):
+    print(i)
