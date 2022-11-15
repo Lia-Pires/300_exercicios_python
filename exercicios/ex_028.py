56d15e6f1d9a4342fa6ca1e142bb2c10b563efed
Lia Pires <liasth.pires@gmail.com>
2022-11-15 14:56:21 -0300

    Adding ex 28

---
+++ exercicios/ex_028.py
@@ -0,0 +1,8 @@
+'''
+28 - Crie uma lista com 8 elementros de uma lista de compras de supermercado, por meio de um laço de repetição for, liste individualmente cada um dos itens dessa lista.
+'''
+
+lista_mercado = ["Pão", "Banana", "Leite", "Alface", "Suco de Laranja", "Maçã", "Carne", "Ovos"]
+
+for itens in lista_mercado:
+    print(itens)
