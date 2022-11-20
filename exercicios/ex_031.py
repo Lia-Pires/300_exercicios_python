0062eea6fcfe36550b6b7504411b221ab8fa8ca6
Lia Pires <liasth.pires@gmail.com>
2022-11-20 11:04:21 -0300

    Adding ex 31

---
+++ exercicios/ex_031.py
@@ -0,0 +1,14 @@
+""" 
+Crie um programa que realiza a progressão aritmética de 20 elementos, com primeiro termo e razão definidos pelo usuário:
+"""
+
+# an = a1 + (n-1) * r
+print("Para criar uma progressão aritmética de 20 elementos: ")
+
+a = int(input("Digite o primeiro termo: "))
+r = int(input("Digite a razão: "))
+
+
+for n in range(1, 21):
+    an = a + (n - 1) * r
+    print(an)
