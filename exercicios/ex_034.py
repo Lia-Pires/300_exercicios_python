5d18bd0dc8ce310e5185eb91b650d177bf33de6b
Lia Pires <liasth.pires@gmail.com>
2022-11-20 11:41:30 -0300

    Adding ex 34

---
+++ exercicios/ex_034.py
@@ -0,0 +1,15 @@
+'''
+34 - Crie um programa que realiza a contagem de 1 até 100, usando apenas números
+ ímpares, ao final do processo exiba em tela quantos números ímpares foram encontrados 
+ nesse intervalo, assim como a soma dos mesmos:
+'''
+
+cont = 0
+soma = 0
+
+for i in range(1, 101, 2):
+    cont += 1
+    soma += i
+
+print(f"Entre 1 e 100 existem {cont} números ímpares")
+print(f"A soma de números ímpares entre 1 e 100 é: {soma}")
