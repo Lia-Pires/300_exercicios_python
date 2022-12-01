09981dd5dca341a544764067b4f65d4837559055
Lia Pires <liasth.pires@gmail.com>
2022-12-01 09:48:53 -0300

    Adding ex 37

---
+++ exercicios/ex_037.py
@@ -0,0 +1,9 @@
+"""
+37 - Declare uma variável que por sua vez recebe um nome digitado pelo usuário, 
+em seguida, exiba em tela uma mensagem de boas vindas que incorpora o nome anteriormente digitado, 
+fazendo o uso de f'strings
+"""
+
+nome = input("Digite seu nome: ")
+
+print(f"Bom dia {nome}, tenha um ótimo dia! ")
