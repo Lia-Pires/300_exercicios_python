408fec6fa8ad29c2c1584aa14f4adaae79eec8bd
Lia Pires <liasth.pires@gmail.com>
2022-12-05 09:11:44 -0300

    Adding ex 39

---
+++ exercicios/ex_038.py
@@ -0,0 +1,9 @@
+"""
+38 - Peça para que o usuário digite um número, diretamente dentro da função print()
+eleve esse número ao quadrado, exibindo o resultado incorporado a uma mensagem:
+
+"""
+
+num = int(input("Digite um número: "))
+
+print(f"{num} elevado ao quadrado resulta {num**2}")
---
+++ exercicios/ex_039.py
@@ -0,0 +1,8 @@
+"""
+39 - Dada a seguinte lista: nomes = ['Ana', 'Carlos', 'Daiane', 'Fernando', 'Maria'] substitua o terceiro elemento da lista por Jamile: ]
+"""
+
+nomes = ["Ana", "Carlos", "Daiane", "Fernando", "Maria"]
+print(nomes)
+nomes[2] = "Jamile"
+print(nomes)
