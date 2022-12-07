99b31a17c0797ceff0485f619ba75e3e227d9add
Lia Pires <liasth.pires@gmail.com>
2022-12-07 08:52:16 -0300

    Adding ex 40

---
+++ exercicios/ex_040.py
@@ -0,0 +1,9 @@
+"""
+40 - Adicione o elemento "Paulo"na lista de nomes
+['Ana', 'Carlos', 'Jamile', 'Fernando', 'Maria']
+"""
+
+nomes = ["Ana", "Carlos", "Jamile", "Fernando", "Maria"]
+print(nomes)
+nomes.append("Paulo")
+print(nomes)
