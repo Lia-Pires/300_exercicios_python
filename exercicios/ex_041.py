a72dfddb1d33ceaeae33726277478f1ae2e8b76b
Lia Pires <liasth.pires@gmail.com>
2022-12-12 20:22:06 -0300

    Adding ex 41

---
+++ exercicios/ex_041.py
@@ -0,0 +1,8 @@
+"""
+Adicione o elementro "Eliana"na lista de nomes, especificamente na terceira posição
+"""
+
+nomes = ["Ana", "Carlos", "Jamile", "Fernando", "Maria", "Paulo"]
+
+nomes.insert(2, "Eliana")
+print(nomes)
