b7c495971d979a50b861b45a3c72d1f75c3151b5
Lia Pires <liasth.pires@gmail.com>
2022-12-14 16:23:18 -0300

    Adding ex 43

---
+++ exercicios/ex_043.py
@@ -0,0 +1,9 @@
+"""
+Mostre o segundo, terceiro e quarto elemento da lista nomes.
+Separadamente, mostre apenas o último elemento da lista nomes:
+"""
+
+nomes = ["Ana", "Eliana", "Jamile", "Fernando", "Maria", "Paulo"]
+
+print(nomes[1:4])
+print(nomes[-1])
