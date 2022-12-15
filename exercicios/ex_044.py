2e33e7c3dedb12c162d7d7e987fe61b035b7eb0e
Lia Pires <liasth.pires@gmail.com>
2022-12-15 20:46:52 -0300

    Adding ex 44

---
+++ exercicios/ex_044.py
@@ -0,0 +1,12 @@
+"""
+44 - Crie um dicionário via método construtor dict(), atribuindo para o mesmo ao menos 5 conjuntos de chaves e valores, representando objetos e seus respectivos preços
+"""
+
+dicionario = dict(
+    TV="R$ 1999,99",
+    Computador="R$ 5070,90",
+    Teclado="R$ 499,00",
+    Mouse="R$ 250,00",
+    Monitor="R$ 1250.50",
+)
+print(dicionario)
