e65f9ff6ad7f1f1676bbf5ee654f474ca5c96770
Lia Pires <liasth.pires@gmail.com>
2022-12-15 20:54:05 -0300

    Adding ex 45

---
+++ exercicios/ex_045.py
@@ -0,0 +1,13 @@
+"""
+45 - A partir da seguinte lista ["C", "JavaScript", "Lua", "Python"] verifique primeiramente e retorne ao usuário se a linguagem de programação Python consta na lista.
+retorne uma mensagem amigável ao usuário para estas duas situações.
+"""
+
+lista = ["C", "JavaScript", "Lua", "Python"]
+
+
+for item in lista:
+    if item == "Python":
+        print("Python está na lista!")
+    else:
+        pass
