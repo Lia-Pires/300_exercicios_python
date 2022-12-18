7901f82843d3e9f418f361915c0f48a4ebd1f6f9
Lia Pires <liasth.pires@gmail.com>
2022-12-18 12:05:51 -0300

    Adding ex 47

---
+++ exercicios/ex_047.py
@@ -0,0 +1,11 @@
+"""
+47 - Crie um dicionário usando o construtor de dicionários do Python,
+alimente os valores do mesmo com dados de duas listas:
+"""
+
+chaves = ["Banana", "Pão", "Frango", "Alface"]
+valores = ["0,99", "9,00", "24,50", "5,00"]
+
+dicionario = dict(keys=chaves, values=valores)
+print(type(dicionario))
+print(dicionario)
