cd1d326d042aa65add0e2557e513f7d68821454c
Lia Pires <liasth.pires@gmail.com>
2022-12-19 19:53:33 -0300

    Adding ex 48

---
+++ exercicios/ex_048.py
@@ -0,0 +1,16 @@
+"""
+48 - Crie uma simples estrutura de dados simulando um cadastro para uma loja.
+nesse cadastro deve conter informações como nome, idade, sexxo, estado civil, nacionalidade, faixa de renda, etc...
+Exiba em tela tais dados.
+"""
+
+cadastro = {
+    "Nome": "Carla",
+    "Sexo": "Feminino",
+    "Idade": "30",
+    "Nacionalidade": "Brasileira",
+    "Estado Civil": "Casada",
+    "Escolaridade": ["Ensino superior", "Mestrado", "Doutorado"],
+    "Ocupação": "Engenheira de Software",
+    "Renda": "5000.00 - 7000.00",
+}
