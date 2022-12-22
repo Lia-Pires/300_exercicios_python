aadd7830e5132734d213943e9b2ccd8461d316ba
Lia Pires <liasth.pires@gmail.com>
2022-12-21 21:12:07 -0300

    Adding ex 50

--- exercicios/ex_050.py
+++ exercicios/ex_050.py
@@ -4,3 +4,51 @@ pedindo que o mesmo insira uma resposta.
 Caso a primeira questào esteja correta, exiba em tela uma mensagem de acerto
 e parta para a próxima pergunta, caso incorreta, exiba uma mensagem de erro e pule para a próxima pergunta:
 """
+
+
+base = {
+    "Pergunta 01": {
+        "Pergunta": "Quanto é 2+2?",
+        "Alternativas": {"a": "3", "b": "5", "c": "4", "d": "9"},
+        "Resposta_correta": "c",
+    },
+    "Pergunta 02": {
+        "Pergunta": "Quanto é 3x3?",
+        "Alternativas": {"a": "9", "b": "6", "c": "1", "d": "7"},
+        "Resposta_correta": "a",
+    },
+    "Pergunta 03": {
+        "Pergunta": "Quanto é 2^2?",
+        "Alternativas": {"a": "9", "b": "12", "c": "4", "d": "13"},
+        "Resposta_correta": "c",
+    },
+    "Pergunta 04": {
+        "Pergunta": "Quanto é 9x9?",
+        "Alternativas": {"a": "81", "b": "64", "c": "98", "d": "99"},
+        "Resposta_correta": "a",
+    },
+}
+
+
+respostas_corretas = 0
+
+for pkeys, pvalues in base.items():
+    print(f'{pkeys}: {pvalues["Pergunta"]}')
+
+    for rkeys, rvalues in pvalues["Alternativas"].items():
+        print(f"[{rkeys}]:{rvalues}")
+
+    resposta = input("Escolha uma alternativa: a, b, c ou d\n")
+
+    if resposta == pvalues["Resposta_correta"]:
+        print("Resposta Correta!!")
+        respostas_corretas += 1
+    else:
+        print("Resposta incorreta!")
+
+    if respostas_corretas == 0:
+        print("Você não acertou nenhuma pergunta!")
+    elif respostas_corretas == 1:
+        print("Você acertou uma questão!")
+    else:
+        print(f"Você acertou {respostas_corretas} questões!")
